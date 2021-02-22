8f6ee3fd315c8e3b0ce6df6dafe4e94df3799416
chrisbenz <cpbenz@protonmail.com>
2021-02-21 22:06:05 -0700

    Committing guessing game code from ch-2 of Rust intro

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,15 +1,33 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
-    println!("Hello, world!");
 
-    println!("Input a number to designate your guess: ");
-
-    let mut guess = String::new(); // Guess is mutable 
+    // println!("Input a number between 1 and 100 to designate your guess: ");
+    let secret_number = rand::thread_rng().gen_range(1,101);
+    // println!("The number to guesss was: {}", random_num);
+    // let mut guess = String::new(); // Guess is mutable 
     
-    io::stdin()
-       .read_line(&mut guess)
-       .expect("Failed to read line");
+    // io::stdin()
+    //    .read_line(&mut guess)
+    //    .expect("Failed to read line");
 
-    println!("You guessed {}", guess);
+    // println!("You guessed {}", guess);
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line from stdin");
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    println!("You guessed the number: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
