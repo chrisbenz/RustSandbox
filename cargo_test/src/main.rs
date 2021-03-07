0a5e3210a2e8bd90c1fa2d59b56be4e57186e9fa
chrisbenz <cpbenz@protonmail.com>
2021-03-07 13:44:50 -0700

    playing around with different data types in Rust, char, int, etc.

--- cargo_test/src/main.rs
+++ cargo_test/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    let c = 'C';
+    let b = 'B';
+    let joy = '😂';
+    let a = 'あ';
+
+    println!("Different characters like {}, {}, {}, and {} are supported within Rust.", c, b, joy, a);
+
+    let tup = (20000, 7.77, 23);
+    let (x,y,z) = tup;
+    println!("You can destructure tuples like so: {}, {}, and {}", x, y, z);
+
+    let x: (&str, u8) = ("Sample Tuple", 1);
+    let name: &str = x.0;
+    let num: u8 = x.1;
+    println!("{} and {}", name, num);
+
 }
