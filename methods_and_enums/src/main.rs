ff484ae19de9989a7e0ff49b830d3b86f729fd45
chrisbenz <cpbenz@protonmail.com>
2021-09-21 23:29:19 -0700

    working with more enums

--- methods_and_enums/src/main.rs
+++ methods_and_enums/src/main.rs
@@ -1,7 +1,20 @@
+use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
+
 #[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
+}
+
+#[derive(Debug)]
+enum IppAddrType {
+    V4,
+    V6
+}
+
+enum IppAddrType_Concise {
+    V4(String),
+    V6(String)
 }
 
 // Methods are defined within the context of a struct.
@@ -30,4 +43,12 @@ fn main() {
     let rect2 = (80, 100);
     println!("Area is {}", area2(&rect1));
     println!("{:#?}", &rect1);
+
+    // Defining new enum types
+    let four = IppAddrType::V4;
+    let six = IppAddrType::V6;
+
+    let localhost_v4 = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
+    println!("{}", localhost_v4.is_ipv4());
+    let concise_ip = IppAddrType_Concise::V4(String::from("::1"));
 }
