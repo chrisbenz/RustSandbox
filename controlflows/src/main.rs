d5df538db6603498a48526c47914cb0e9a476dc6
chrisbenz <cpbenz@protonmail.com>
2021-09-25 22:04:28 -0700

    small additional control flow examples

--- controlflows/src/main.rs
+++ controlflows/src/main.rs
@@ -22,6 +22,27 @@ fn main() {
 
     // Rust has no nulls, instead an enum to encode a value being present or absent
     let missing_number: Option<i32> = None;
+
+    let some_u8_val = Some(0u8);
+    if let Some(3) = some_u8_val {
+        println!("Val is equal to three");
+    }
+
+    let mut count = 0;
+
+    let some_coin = Coin::Quarter(State::Arizona);
+    
+    // match some_coin {
+    //     Coin::Quarter(state) => println!("State quarter from the {:?} state", state),
+    //     _ => count += 1,
+    // }
+
+    // Same thing as above but with if let else
+    if let Coin::Quarter(state) = some_coin {
+        println!("State quarter from {:?}!", state);
+    } else {
+        count += 1;
+    }
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
